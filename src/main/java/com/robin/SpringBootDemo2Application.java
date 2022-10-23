a34988e9787d28e9532e742939b58b8f4d528dd0
Robin.J.Y.ZHONG <15914468806@139.com>
2022-10-24 00:11:29 +0800

    MultipartConfigElement filter

--- src/main/java/com/robin/SpringBootDemo2Application.java
+++ src/main/java/com/robin/SpringBootDemo2Application.java
@@ -1,7 +1,12 @@
 package com.robin;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class SpringBootDemo2Application {
@@ -9,5 +14,15 @@ public class SpringBootDemo2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemo2Application.class, args);
 	}
-
+	
+	// Bean拦截器
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {  
+	    MultipartConfigFactory factory = new MultipartConfigFactory();  
+	    //单个文件最大
+	    factory.setMaxFileSize(DataSize.ofMegabytes(10)); // 10MB, 10 * 1024KB  
+	    // 设置总上传数据总大小
+	    factory.setMaxRequestSize(DataSize.ofMegabytes(1000));  // 1000MB, 1000 * 1024KB
+	    return factory.createMultipartConfig();  
+	}
 }
